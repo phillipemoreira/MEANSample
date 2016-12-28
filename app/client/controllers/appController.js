7205a1183040b7226f78080f16577dc1fad13a56
phillipeamoreira <phillipealexmoreira@gmail.com>
2016-12-28 17:37:46 -0200

    Getting dummy data from node server at /contactList

--- app/client/controllers/appController.js
+++ app/client/controllers/appController.js
@@ -1,3 +1,9 @@
-app.controller('appController', function() {
+app.controller('appController', function($scope, $http) {
 	console.log("Hello world from controller");
+	
+	$http.get('/contactlist').then(function(response){
+		console.log("I got the data I requested");
+		$scope.contactList = response.data;
+	});
+	
 });
--- app/server.js
+++ app/server.js
@@ -3,5 +3,31 @@ var app = express();
 
 app.use(express.static(__dirname + "/client"));
 
+app.get('/contactlist', function(req, res) {
+	console.log("I received a GET request.");
+	
+	//Dummy data
+	person1 = {
+		name : 'Tim',
+		email : 'tim@gmail.com',
+		number: '(222) 222-2222'
+	};
+	
+	person2 = {
+		name : 'Gloria',
+		email : 'gloria@gmail.com',
+		number: '(333) 333-3333'
+	};
+	
+	person3 = {
+		name : 'John',
+		email : 'john@gmail.com',
+		number: '(444) 444-4444'
+	};
+	
+	var contactList = [person1, person2, person3];
+	res.json(contactList);
+});
+
 app.listen(3000);
 console.log("Server running in port 3000");
