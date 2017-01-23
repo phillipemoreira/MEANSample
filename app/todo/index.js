6aa8f3058e6699bb390ad46147c5a3d266d945ec
Phillipe Moreira <phillipealexmoreira@gmail.com>
2017-01-22 23:04:09 -0200

    todo first index page

---
+++ app/todo/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 3001;
+
+app.all('/*', function(req, res){
+	res.send("\
+		<!DOCTYPE html>\
+		<html>\
+			<head>\
+				<title>Mean TODO app</title>\
+			</head>\
+			<body>\
+				<h1>Hello this is the APP</h1>\
+			</body>\
+		</html>\
+		");
+});
+
+app.listen(PORT, function(){
+	console.log("server running on " + PORT);
+});
